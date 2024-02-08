4d5a7b4e25ed6c01361f00ca04b1a54f674b8ffa
taniya-ignatius <itaniya895@gmail.com>
2024-02-09 00:04:59 +0530

    Patients viewed successfully

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPatient from './components/AddPatient';
+import ViewPatients from './components/ViewPatients';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<AddPatient/>}/>
+        <Route path='/view' element={<ViewPatients/>}/>
       </Routes>
       </BrowserRouter>
     </div>
